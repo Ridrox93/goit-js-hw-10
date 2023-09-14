3c9d566478c55cb4d0222c9d841b8b30671df432
Ridrox93 <popovandrii@yahoo.com>
2023-09-14 19:37:45 +0200

    5-add

---
+++ src/cat-api.js
@@ -0,0 +1,32 @@
+const BASE_URL = `https://api.thecatapi.com/v1/`;
+
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_711r8yTv9xG2fO9BQTLfum7QEckQQCT3OFOGlcwBlvo8Km84wEsjWYirfj4xrscp'";
+
+export function fetchBreeds() {
+    return axios.get(`${BASE_URL}/breeds`)
+        .then(response => {
+           
+            return response.data;
+        })
+        .catch(error => {
+
+            throw new Error("Помилка запиту:", error.message);
+        });
+
+}
+
+
+export function fetchCatByBreed(breedId) {
+    return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+        .then(response => {
+          
+            return response.data;
+        })
+        .catch(error => {
+
+            throw new Error("Помилка запиту:", error.message);
+        });
+
+}
